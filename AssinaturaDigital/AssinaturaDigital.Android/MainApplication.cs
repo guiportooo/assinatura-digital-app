347929dc472ebf80938343e17cdc1774005bb031
Robson Amorim <robson.amorim@lambda3.com.br>
2019-05-16 19:28:39 +0000

    Merged PR 83: Adicionar plugin CrossCurrentActivity no projeto Android
    Adicionar plugin CrossCurrentActivity no projeto Android

---
+++ AssinaturaDigital/AssinaturaDigital.Android/MainApplication.cs
@@ -0,0 +1,25 @@
+using Android.App;
+using Android.Runtime;
+using Plugin.CurrentActivity;
+using System;
+namespace AssinaturaDigital.Droid
+{
+#if DEBUG
+    [Application(Debuggable = true)]
+#else
+    [Application(Debuggable = false)]
+#endif
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer)
+          : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            CrossCurrentActivity.Current.Init(this);
+        }
+    }
+}
--- AssinaturaDigital/AssinaturaDigital/ViewModels/SuccessSigningContractViewModel.cs
+++ AssinaturaDigital/AssinaturaDigital/ViewModels/SuccessSigningContractViewModel.cs
@@ -4,7 +4,6 @@ using AssinaturaDigital.Views;
 using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
-using System;
 using Xamarin.Essentials.Interfaces;
 
 namespace AssinaturaDigital.ViewModels
